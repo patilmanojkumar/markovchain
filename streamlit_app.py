368d9a06b3695e9e5a901f93b6aed18b700f516b
Manojkumar Patil <88649192+patilmanojkumar@users.noreply.github.com>
2025-03-26 09:39:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ import pandas as pd
 import numpy as np
 import io
 st.markdown("""
-VBA Code Developed by: Late B. N. Raviprakash
-Translated to Python by: Manojkumar Patil
-Description: This Streamlit-based application generates Markov Chain equations from user-uploaded Excel or CSV files.
+**VBA Code Developed by:** Late B. N. Raviprakash  
+**Translated to Python by:** Manojkumar Patil  
+**Description:** This Streamlit-based application generates Markov Chain equations from user-uploaded Excel or CSV files.  
 """)
 class MarkovCalculator:
     def __init__(self):
